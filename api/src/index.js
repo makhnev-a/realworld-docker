488f2046f208d7060e19db1520a3896e3fe6bb14
Makhnev Andrey <ray.makhnev@gmail.com>
2020-09-27 12:48:22 +0300

    post && get query to db

--- api/src/index.js
+++ api/src/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const {connectDb} = require('./helpers/db');
+const mongoose = require('mongoose');
 const {port, host, db} = require('./configuration');
+const postSchema = new mongoose.Schema({
+  name: String
+});
+const Post = new mongoose.model('Post', postSchema);
 
 const app = express();
 
@@ -9,6 +14,17 @@ const startServer = () => {
     console.log(`Started api services! on port ${port}`);
     console.log(`On host ${host}`);
     console.log(`Our database ${db}`)
+
+    // Post.find(function (err, posts) {
+    //   if (err) return console.log(err);
+    //   console.log('posts', posts);
+    // });
+
+    const silence = new Post({name: "Silence"});
+    silence.save(function (err, savedSilence) {
+      if (err) return console.log('error', err);
+      console.log('savedSilence', savedSilence);
+    })
   });
 };
 
